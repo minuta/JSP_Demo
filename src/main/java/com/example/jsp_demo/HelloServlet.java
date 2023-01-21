82f912d66e1ec514ad4f52217b50e3561cac1652
minuta <gammamustang@gmail.com>
2023-01-21 19:22:44 +0100

    add Log4j2

--- src/main/java/com/example/jsp_demo/HelloServlet.java
+++ src/main/java/com/example/jsp_demo/HelloServlet.java
@@ -1,13 +1,21 @@
 package com.example.jsp_demo;
 
-import java.io.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+
+    private static final Logger LOGGER = LogManager.getLogger("HelloServlet");
+
 
     @Override
     public void init() {
@@ -16,9 +24,11 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        LOGGER.error("FFFF ---> entering doGet in HelloServlet");
+
         response.setContentType("text/html");
 
-        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,14 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>landing page</title>
 </head>
 <body>
 
     <h1><%= "Landing page" %> </h1>
     <br/>
 
-    <a href="hello-servlet">call servlet</a>
+    <a href="hello-servlet">call me a servlet</a>
 
 </body>
 </html>
